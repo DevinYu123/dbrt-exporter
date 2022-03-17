b6f9a9fc2c5fe3769a5a27fb8ec638555966236b
DevinYu123 <37359791+DevinYu123@users.noreply.github.com>
2022-03-17 16:28:35 +0800

    v1.1

--- init.sql
+++ init.sql
@@ -1,20 +1,11 @@
 #MongoDB
-use xxx
-db.createUser({
-user: "dbrt",
-pwd: "dbrt",
-roles: [
-{"role" : "readWrite", "db" : "xxx"},
-]
-})
-db.dbrt.insert({})
+use dbrt;
+db.dbrt.insert({'dbrt': NumberLong(1)});
+db.createUser({user: 'dbrt', pwd: 'dbrt', roles: [{'role' : 'readWrite', 'db' : 'dbrt'},]});
 
 #MySQL
-create user dbrt@'172.21.%' identified by 'dbrt';
-grant select,insert,update,delete on xxx.* to dbrt@'172.21.%';
-create database xxx;
-CREATE TABLE xxx.dbrt (
-  `id` int(11) NOT NULL DEFAULT '0',
-  `dbrt_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
+create database dbrt;
+create table dbrt.dbrt (id int(11) NOT NULL DEFAULT '0',dbrt_time datetime DEFAULT NULL,PRIMARY KEY (id));
+insert into dbrt.dbrt value(1,now());
+create user dbrt@'%' identified by 'dbrt';
+grant select,insert,update,delete on dbrt.* to dbrt@'%';
